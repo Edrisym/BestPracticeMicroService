b084aa214e87fc2587be689beea6e21b06010eac
EdrisGhafori <edrismoavenghafouri@gmail.com>
2023-10-18 10:30:29 +0330

    Modifying the records and ading some validation

--- Play.Catalog/src/Play.Catalog.Service/Dtos.cs
+++ Play.Catalog/src/Play.Catalog.Service/Dtos.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,8 +9,8 @@ namespace Play.Catalog.Service
 {
     public class Dtos
     {
-        public record ItemDto(Guid Id , string Name , string Description,  decimal Price , DateTimeOffset CreatedDate);
-        public record CreateItemDto(string Name , string Description , decimal Price);
-        public record UpdateItemDto(string Name , string Description , decimal Price);
+        public record ItemDto(Guid Id ,string Name , string Description,  decimal Price , DateTimeOffset CreatedDate);
+        public record CreateItemDto( [Required]string Name , string Description , [Range(0,10000)] decimal Price);
+        public record UpdateItemDto( [Required]string Name , string Description , [Range(0,10000)] decimal Price);
     }
 }
