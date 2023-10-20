379f1fae8bbc75cf1001dfc0d4140c2ff26787bf
Edrisym <edrismoavenghafouri@gmail.com>
2023-10-20 16:00:51 +0330

    adding extensions mapper

---
+++ Play.Catalog/src/Play.Catalog.Service/Extensions.cs
@@ -0,0 +1,13 @@
+using Play.Catalog.Service.Entities;
+using static Play.Catalog.Service.Dtos;
+
+namespace Play.Catalog.Service
+{
+    public static class Extensions
+    {
+        public static ItemDto AsDto(this Item item)
+        {
+            return new ItemDto(item.Id, item.Name, item.Description, item.Price, item.CreatedDate);
+        }
+    }
+}
