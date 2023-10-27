cd14c6e4e09a6b56862c4dbc42cee3a880d105ad
Edrisym <edrismoavenghafouri@gmail.com>
2023-10-27 18:25:58 +0330

    Modifying Program for adding connectinString

--- Play.Catalog/src/Play.Catalog.Service/Program.cs
+++ Play.Catalog/src/Play.Catalog.Service/Program.cs
@@ -1,12 +1,36 @@
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Bson;
+using System.Runtime.CompilerServices;
+using Play.Catalog.Service.Settings;
+using MongoDB.Driver;
+using Play.Catalog.Service.Repositories;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//builder.Services.Configure<ServiceSettings>(
+  //  builder.Configuration.GetSection(nameof(ServiceSettings)));
+
+var serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+
+builder.Services.AddSingleton(ServiceProvider => {
+
+    var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+    var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
+    return mongoClient.GetDatabase(serviceSettings.ServiceName);
+});
+
+builder.Services.AddSingleton<IItemsRepository , ItemsRepository>();
+
+// Add services to the container.
+
 BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
 BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
 builder.Services.AddControllers(
     option => { option.SuppressAsyncSuffixInActionNames = false; }
 );
